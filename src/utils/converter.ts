2f3e87e54c054afa0b6510f6a8db7954b032f571
whitedark9 <blancooscuro.mm@gmail.com>
2025-11-17 23:59:08 +0630

    'update'

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,17 +1,3 @@
-export function convertToChartData(arr: any[], labelName: string) {
-  return {
-    data: {
-      labels: arr.map((item) => item.value),
-      datasets: [
-        {
-          label: labelName,
-          data: arr.map((item) => item.frequency),
-        },
-      ],
-    },
-  };
-}
-
 export function cleanName(name: string) {
   return name.replaceAll("_", " ");
 }
